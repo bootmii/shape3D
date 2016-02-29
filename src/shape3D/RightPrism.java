7f053002b20530474e54b78b262181b43d17cc0f
116060088 <alt.people.davidcalman@gmail.com>
2016-02-29 13:28:09 -0800

    fixed `RightPrism` method to work with `Cube`

--- src/shape3D/RightPrism.java
+++ src/shape3D/RightPrism.java
@@ -2,7 +2,7 @@ package shape3D;
 
 public abstract class RightPrism implements Shape3D {
 	private double height;
-	public double getVolume() {return height*this.getBaseArea();}
+	public double getVolume() {return this.getHeight()*this.getBaseArea();}
 	public abstract double getSurfaceArea();
 	public double getHeight() {return height;}
 	public abstract double getBaseArea();
