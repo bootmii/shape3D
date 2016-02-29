845c3c449be3b6d22b4897eebef0cfc200238c76
116060088 <alt.people.davidcalman@gmail.com>
2016-02-29 08:58:56 -0800

    Cube now has all methods implemented

--- src/shape3D/Cube.java
+++ src/shape3D/Cube.java
@@ -1,5 +1,8 @@
 package shape3D;
 
 public class Cube extends RectPrism implements Shape3D {
-	
+	private double side;
+	public double getSurfaceArea() { return 6*side*side; }
+	public double getHeight() { return side; }
+	public double getBaseArea() { return side*side; }
 }
