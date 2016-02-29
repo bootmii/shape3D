5e123d6ee4bd5fe59e86f731c108fa0d2ce7e33b
116060088 <alt.people.davidcalman@gmail.com>
2016-02-29 09:02:19 -0800

    Cylinder class

---
+++ src/shape3D/Cylinder.java
@@ -0,0 +1,14 @@
+package shape3D;
+
+public class Cylinder extends RightPrism {
+	private double height;
+	private double radius;
+	
+	public double getSurfaceArea() {
+		return 2*Math.PI*radius*(height+radius);
+	}
+	public double getBaseArea() {
+		return Math.PI*radius*radius;
+	}
+
+}
--- src/shape3D/RectPrism.java
+++ src/shape3D/RectPrism.java
@@ -4,12 +4,9 @@ public class RectPrism extends RightPrism implements Shape3D {
 	private double length, width, height;
 
 	public double getSurfaceArea() {
-		// TODO Auto-generated method stub
 		return 2*((length*width)+(width*height)+(length*height));
 	}
-
 	public double getBaseArea() {
-		// TODO Auto-generated method stub
 		return length*width;
 	}
 
