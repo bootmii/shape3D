fb41b2b0f4152b72b842d85dea49360367f87a47
116060088 <alt.people.davidcalman@gmail.com>
2016-02-29 09:02:19 -0800

    Cylinder class

--- src/shape3D/RightPrism.java
+++ src/shape3D/RightPrism.java
@@ -1,6 +1,6 @@
 package shape3D;
 
-public abstract class RightPrism {
+public abstract class RightPrism implements Shape3D {
 	private double height;
 	public double getVolume() {return height*this.getBaseArea();}
 	public abstract double getSurfaceArea();
